9e368692d83b4b5cc97f106e30ad6711e5eab091
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-10 19:59:33 +0530

    basic express code

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('store-api');
+require('dotenv').config();
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    // connect DB
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
