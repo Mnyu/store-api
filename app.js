d98afa393a5582d134fce5555556ed3921ff8320
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-10 20:43:17 +0530

    db connection code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,12 +14,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
 });
 
+// middlewares
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
-    // connect DB
+    await connectDB(process.env.MONGO_URI); // connect DB
     app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
   } catch (error) {
     console.log(error);
